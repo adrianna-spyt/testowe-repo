a7b43a0cfbcd4bc4a528b5006d987c5eefac84db
adrianna-spyt <71889255+adrianna-spyt@users.noreply.github.com>
2022-05-04 10:11:02 +0200

    Update f2.py

--- f2.py
+++ f2.py
@@ -34,35 +34,35 @@ print(usunnieparzyste(lista1))
 print(usunnieparzyste(lista2))
 print(usunnieparzyste(lista3))
 
-# def alternatywa_rozlaczna(zmiennaP, zmiennaQ):
-#     if (poprawnosc(zmiennaP) and poprawnosc(zmiennaQ))== "Zmienna jest poprawna.":
-#         if (zmiennaP == 1 and zmiennaQ == 0) or (zmiennaP == 0 and zmiennaQ == 1):
-#             return "True"
-#         else:
-#             return "False"
-#     else:
-#         return "Brak odpowiedzi - błędna zmienna."
-#
-#
-# def poprawnosc(zmienna):
-#     if zmienna == 1 or zmienna == 0:
-#         return "Zmienna jest poprawna."
-#     else:
-#         return "Zmienna jest niepoprawna."
-#
-#
-# ptrue = 1
-# pfalse = 0
-# qtrue = 1
-# qfalse = 0
-# print("Dla p =", ptrue, "-", poprawnosc(ptrue), "\nI dla q =", qtrue, "-", poprawnosc(qtrue))
-# print("Wynikiem alternatywy rozłącznej jest", alternatywa_rozlaczna(ptrue, qtrue))
-# print("Dla p =", ptrue, "-", poprawnosc(ptrue), "\nI dla q =", qfalse, "-", poprawnosc(qfalse))
-# print("Wynikiem alternatywy rozłącznej jest", alternatywa_rozlaczna(ptrue, qfalse))
-# print("Dla p =", pfalse, "-", poprawnosc(pfalse), "\nI dla q =", qtrue, "-", poprawnosc(qtrue))
-# print("Wynikiem alternatywy rozłącznej jest", alternatywa_rozlaczna(pfalse, qtrue))
-# print("Dla p =", pfalse, "-", poprawnosc(pfalse), "\nI dla q =", qfalse, "-", poprawnosc(qfalse))
-# print("Wynikiem alternatywy rozłącznej jest", alternatywa_rozlaczna(pfalse, qfalse))
-#
-# print("Dla p =", 7, "-", poprawnosc(7), "\nI dla q =", -3, "-", poprawnosc(-3))
-# print("Wynikiem alternatywy rozłącznej jest", alternatywa_rozlaczna(7, -3))
+def alternatywa_rozlaczna(zmiennaP, zmiennaQ):
+    if (poprawnosc(zmiennaP) and poprawnosc(zmiennaQ))== "Zmienna jest poprawna.":
+        if (zmiennaP == 1 and zmiennaQ == 0) or (zmiennaP == 0 and zmiennaQ == 1):
+            return "True"
+        else:
+            return "False"
+    else:
+        return "Brak odpowiedzi - błędna zmienna."
+
+
+def poprawnosc(zmienna):
+    if zmienna == 1 or zmienna == 0:
+        return "Zmienna jest poprawna."
+    else:
+        return "Zmienna jest niepoprawna."
+
+
+ptrue = 1
+pfalse = 0
+qtrue = 1
+qfalse = 0
+print("Dla p =", ptrue, "-", poprawnosc(ptrue), "\nI dla q =", qtrue, "-", poprawnosc(qtrue))
+print("Wynikiem alternatywy rozłącznej jest", alternatywa_rozlaczna(ptrue, qtrue))
+print("Dla p =", ptrue, "-", poprawnosc(ptrue), "\nI dla q =", qfalse, "-", poprawnosc(qfalse))
+print("Wynikiem alternatywy rozłącznej jest", alternatywa_rozlaczna(ptrue, qfalse))
+print("Dla p =", pfalse, "-", poprawnosc(pfalse), "\nI dla q =", qtrue, "-", poprawnosc(qtrue))
+print("Wynikiem alternatywy rozłącznej jest", alternatywa_rozlaczna(pfalse, qtrue))
+print("Dla p =", pfalse, "-", poprawnosc(pfalse), "\nI dla q =", qfalse, "-", poprawnosc(qfalse))
+print("Wynikiem alternatywy rozłącznej jest", alternatywa_rozlaczna(pfalse, qfalse))
+
+print("Dla p =", 7, "-", poprawnosc(7), "\nI dla q =", -3, "-", poprawnosc(-3))
+print("Wynikiem alternatywy rozłącznej jest", alternatywa_rozlaczna(7, -3))
